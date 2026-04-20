88dcba804475478efb92e3f70b76ba99f87d021b
D3ITY-YODA <brendanakudo@gmail.com>
2026-04-20 12:35:35 +0000

    Add routes/web.php content

--- leave-app/routes/web.php
+++ leave-app/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeaveController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LeaveController::class, 'index'])->name('leaves.index');
+Route::post('/leaves', [LeaveController::class, 'store'])->name('leaves.store');
+Route::delete('/leaves/{leaveApplication}', [LeaveController::class, 'destroy'])->name('leaves.destroy');
