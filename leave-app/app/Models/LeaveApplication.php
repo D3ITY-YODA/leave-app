f959e3c13558faa60c030d4c1154b417a4380ff9
D3ITY-YODA <brendanakudo@gmail.com>
2026-04-20 12:31:20 +0000

    change contents of LeaveApplication.php

--- leave-app/app/Models/LeaveApplication.php
+++ leave-app/app/Models/LeaveApplication.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeaveApplication extends Model
 {
-    //
+    protected $fillable = [
+        'employee_name',
+        'leave_type',
+        'start_date',
+        'end_date',
+        'days',
+        'reason',
+        'status',
+    ];
 }
