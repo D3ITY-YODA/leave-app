a23a37e08e6a9e2bffeb7f7c84b551c3dde16d0a
D3ITY-YODA <brendanakudo@gmail.com>
2026-05-18 07:51:04 +0000

    Add web.php routes

--- leave-app/routes/web.php
+++ leave-app/routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [LeaveController::class, 'index'])->name('leaves.index');
 Route::post('/leaves', [LeaveController::class, 'store'])->name('leaves.store');
 Route::delete('/leaves/{leaveApplication}', [LeaveController::class, 'destroy'])->name('leaves.destroy');
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+Route::post('/admin/approve/{leaveApplication}', [AdminController::class, 'approve'])->name('admin.approve');
+Route::post('/admin/reject/{leaveApplication}',  [AdminController::class, 'reject'])->name('admin.reject');
